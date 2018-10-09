ede1fbec7683ca99db48adf464592335f46958ab
Hwan Seob Lee <bestseob93@gmail.com>
2018-10-10 00:33:27 +0900

    serverless.yml 설정

--- bestsup-backend/src/blogs.js
+++ bestsup-backend/src/blogs.js
@@ -1,3 +1,8 @@
+const uuid = require('uuid');
+const AWS = reuqire('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 const createResponse = (status, body) => ({
   statusCode: status,
   body: JSON.stringify(body)
@@ -5,7 +10,45 @@ const createResponse = (status, body) => ({
 
 // 블로그 만들기
 exports.createBlog = (event, ctx, cb) => {
-  cb(null, createResponse(200, { message: 'create' }));
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  if (typeof data.text !== 'string') {
+    console.error('Validation Failed');
+    cb(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the todo item.',
+    });
+
+    return;
+  }
+
+  const params = {
+    TableName: 'blog',
+    Item: {
+      id: uuid.v1(),
+      text: data.text,
+      checked: false,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      cb(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the todo item.',
+      });
+      return;
+    }
+    cb(null, createResponse(200, { message: 'create' }));
+  });
 };
 
 // 여러개의 블로그 리스팅
