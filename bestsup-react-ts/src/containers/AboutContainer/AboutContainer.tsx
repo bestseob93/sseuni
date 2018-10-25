9b98bf509cb30ccc5994f2f3cea6f96cb6e325e8
Hwan Seob Lee <bestseob93@gmail.com>
2018-10-25 22:03:21 +0900

    react @16.6.0 upgrade

--- bestsup-react-ts/src/containers/AboutContainer/AboutContainer.tsx
+++ bestsup-react-ts/src/containers/AboutContainer/AboutContainer.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import './AboutContainer.css';
 
 class AboutContainer extends React.Component {
   public render() {
     return (
-      <div>
-        AboutContainer
-      </div>
+      <section className="about-container">
+        <div className="sticky-imgs">
+          <div className="figure-container">
+            <figure className="image-1" />
+            <figure className="image-2" />
+            <figure className="image-3" />
+            <figure className="image-4" />
+            <figure className="image-5" />
+            <figure className="image-6" />
+            <figure className="image-7" />
+          </div>
+        </div>
+      </section>
     );
   }
 }
