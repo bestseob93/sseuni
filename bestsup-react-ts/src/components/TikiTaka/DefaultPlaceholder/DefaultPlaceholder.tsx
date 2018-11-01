6d64fc8abdc916e86c69f4c70ed21ac474a7d1cf
Hwan Seob Lee <bestseob93@gmail.com>
2018-11-02 00:17:42 +0900

    defaultplaceholder 추가 && TODO: ContentEditAble 컴포넌트화 시키기

---
+++ bestsup-react-ts/src/components/TikiTaka/DefaultPlaceholder/DefaultPlaceholder.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const DefaultPlaceholder: React.StatelessComponent<{}> = () => {
+  return (
+    <span>
+      Title
+    </span>
+  );
+}
+
+export default DefaultPlaceholder;
--- bestsup-react-ts/src/components/TikiTaka/TikiTaka.tsx
+++ bestsup-react-ts/src/components/TikiTaka/TikiTaka.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
+import DefaultPlaceholder from './DefaultPlaceholder';
 
 import './TikiTaka.css';
 
+// TODO: ContentEditable 컴포넌트화 시키기
 class TikiTaka extends React.Component<{}> {
   public render() {
     return (
       <div className="tikitaka-editor">
-        <h1 className="title" contentEditable={true}>
-          <span className="default-ph-t">Title</span>
+        <h1 className="title" contentEditable={true} onChange={(e) => console.log(e)}>
+          <DefaultPlaceholder />
         </h1>
         <p contentEditable={true}>
           <span className="default-ph-b">Body</span>
