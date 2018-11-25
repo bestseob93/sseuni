00dab7703fcea2b9c9c594cb322708c6839806a2
Hwan Seob Lee <bestseob93@gmail.com>
2018-11-25 23:44:26 +0900

    added Loading Component

---
+++ bestsup-react-ts/src/components/Common/Loading.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Loading.css';
+
+const Loading: React.StatelessComponent<{}> = () => {
+  return (
+    <div>
+      Loading...
+    </div>
+  );
+}
+
+export default Loading;
