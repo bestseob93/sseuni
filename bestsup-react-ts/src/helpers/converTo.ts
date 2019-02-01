932a5414d91cb31da54d3ed2fd34b03e16367507
Hwan Seob Lee <bestseob93@gmail.com>
2019-02-01 19:47:33 +0900

    change typeof converTo function to any

--- bestsup-react-ts/src/helpers/converTo.ts
+++ bestsup-react-ts/src/helpers/converTo.ts
@@ -3,17 +3,18 @@
  * @param {animation duration} value
  * @param {x, y, z} axis
  */
-export const convertPercentToPx: number = (value: string, axis: string) => {
+export const convertPercentToPx: any = (value: string, axis: string) => {
+  let newValue = parseInt(value, 10);
   if (typeof value === "string" && value.match(/%/g)) {
     if (axis === 'y') {
       // TODO: 추후 고정된 값으로 변경 해보기
-      value = (parseFloat(value) / 100) * window.innerHeight;
+      newValue = (newValue / 100) * window.innerHeight;
     }
     if (axis === 'x') {
-      value = (parseFloat(value) / 100) * window.innerWidth;
+      newValue = (newValue / 100) * window.innerWidth;
     }
   }
-  return value;
+  return newValue;
 }
 
 export const convertedDate = (timestamp: any): string => {
