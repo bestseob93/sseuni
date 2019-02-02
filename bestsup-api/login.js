0f7e1ecaef5a7360828372a7f9c2455196da056c
Hwan Seob Lee <bestseob93@gmail.com>
2019-02-03 00:22:44 +0900

    feat: 'login' method for login

---
+++ bestsup-api/login.js
@@ -0,0 +1,23 @@
+import crypto from 'crypto';
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+
+
+  const params = {
+    TableName: "user",
+  };
+
+  try {
+    const result = await dynamoDbLib.call('put', params);
+    const password = data.password;
+    // const salt = ;
+    // const hashPassword = crypto.createHash('sha512').update(password + salt).digest('hex');
+    callback(null, success(result));
+  } catch (e) {
+    console.log(e);
+    callback(null, failure({ status: false ));
+  }
+}
