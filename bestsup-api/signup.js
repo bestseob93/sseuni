93b2c30472ace194cddcf432716cab545d383d2e
Hwan Seob Lee <bestseob93@gmail.com>
2019-02-07 18:25:03 +0900

    feat: signup json parse 과정 safelyParseJSON 사용

--- bestsup-api/signup.js
+++ bestsup-api/signup.js
@@ -1,19 +1,21 @@
 import uuid from "uuid";
 import crypto from 'crypto';
+import * as s3Lib from './libs/s3-lib';
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
-  const data = JSON.parse(event.body);
-  const password = data.password;
+  console.log(event.body);
+  const data = s3Lib.safelyParseJSON(event.body);
+  const password = data.payload.password;
   const salt = Math.round((new Date().valueOf() * Math.random())) + '';
   const hashPassword = crypto.createHash('sha512').update(password + salt).digest('hex');
 
   const params = {
     TableName: 'user',
     Item: {
-      id: uuid.v4(),
-      username: data.username,
+      id: uuid.v1(),
+      username: data.payload.username,
       password: hashPassword,
       salt: salt
     }
@@ -24,6 +26,6 @@ export async function main(event, context, callback) {
     callback(null, success(params.Item));
   } catch (e) {
     console.log(e);
-    callback(null, failure({ status: false, e, blogId, data: params.Item }));
+    callback(null, failure({ status: false, e: JSON.stringify(e) }));
   }
 }
